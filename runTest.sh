d6a87c9fe69b9b0abbaadf73cfc9005c95d1773c
Adam Reyes <acreyes@code.pas.rochester.edu>
2023-08-24 15:40:48 -0400

    can pass viewarchive directory, branch and testfile as environment variables to docker-compose services for the tester

--- runTest.sh
+++ runTest.sh
@@ -4,26 +4,31 @@
 MYUSER=$1
 VM=$2
 TESTFILE=$3
+BRANCH=$4
+BRANCH=$(echo ${BRANCH} | sed 's;\/;\\\/;')
+VIEWARCH=$5
 
 FLASHTEST_DIR=/home/${MYUSER}/flashTest
 #configure ssh
 #   add flash to known hosts and point ssh to user's id_rsa
 mkdir -p ~/.ssh
-# cp /local/config ~/.ssh
 echo -e "Host *\n    LogLevel QUIET\n    StrictHostKeyChecking no\n    IdentitiesOnly yes\n    IdentityFile /mnt/ssh/id_rsa\n    User ${MYUSER}" > ~/.ssh/config
-# ssh-keyscan -H flash.rochester.edu >> ~/.ssh/known_hosts
-# ssh-keyscan -H code.pas.rochester.edu >> ~/.ssh/known_hosts
-# export SVN_SSH="ssh -i /mnt/ssh/id_rsa"
-
-
 
 # check out and configure flashTest
 svn co svn+ssh://flash.rochester.edu/home/svn/repos/flashTest/trunk ${FLASHTEST_DIR}
 cd ${FLASHTEST_DIR}
+
+# specify the user, the branch, and the path in the view archive
 # replace tester in config file with ${MYUSER}
 sed "s/tester/${MYUSER}/g" ${FLASHTEST_DIR}/VMs/${VM}/config > ${FLASHTEST_DIR}/config
+sed -i "s/@branchname/${BRANCH}/g" ${FLASHTEST_DIR}/config
+sed -i "s/@view/${VIEWARCH}/g" ${FLASHTEST_DIR}/config
+
 ln -s ${FLASHTEST_DIR}/VMs/${VM}/exeScript ./exeScript
-ln -s ${FLASHTEST_DIR}/TestInfoFiles/testComp.gnu-mpich.info ${FLASHTEST_DIR}/test.info
+# ln -s ${FLASHTEST_DIR}/TestInfoFiles/testComp.gnu-mpich.info ${FLASHTEST_DIR}/test.info
+sed "s/gnu-mpich/${VM}/g" ${FLASHTEST_DIR}/TestInfoFiles/testComp.gnu-mpich.info > ${FLASHTEST_DIR}/test.info
+head -1 ${FLASHTEST_DIR}/test.info
+
 mkdir -p /home/${MYUSER}/FLASH4
 
 #rsync the output directory from the volume to local
