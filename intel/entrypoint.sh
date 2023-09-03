7f9b438cb9701f3b13a629bf1d74aa4d8705c77c
Adam Reyes <acreyes@code.pas.rochester.edu>
2023-09-03 12:40:29 -0400

    intel entrypoint updated for new args

--- intel/entrypoint.sh
+++ intel/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #
 MYUSER=$1
-INFOFILE=$2
+VM=$2
+TESTFILE=$3
+BRANCH=$4
+# BRANCH=$(echo ${BRANCH} | sed 's;\/;\\\/;')
+VIEWARCH=$5
 
 source /opt/intel/oneapi/setvars.sh
 
-/local/runTest.sh ${MYUSER} intel ${INFOFILE}
+/local/runTest.sh ${MYUSER} ${VM} ${TESTFILE} ${BRANCH} ${VIEWARCH}
 
 
